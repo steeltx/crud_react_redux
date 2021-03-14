8a976fda086702a526892fd28f7882058d99c094
steeltx <realsteel.gnu@gmail.com>
2021-03-13 20:34:01 -0600

    Obtener productos y mostrarlos

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -1,7 +1,10 @@
 import {
     AGREGAR_PRODUCTO,
     AGREGAR_PRODUCTO_EXITO,
-    AGREGAR_PRODUCTO_ERROR
+    AGREGAR_PRODUCTO_ERROR,
+    COMENZAR_DESCARGA_PRODUCTOS,
+    DESCARGA_PRODUCTOS_EXITO,
+    DESCARGA_PRODUCTOS_ERROR
 } from '../types';
 import clienteAxios from '../config/axios';
 import Swal from 'sweetalert2';
@@ -52,4 +55,32 @@ const agregarProductoExito = producto => ({
 const agregarProductoError = estado => ({
     type: AGREGAR_PRODUCTO_ERROR,
     payload: estado
+});
+
+// funcion que descarga los productos de la base de datos
+export function obtenerProductosAction(){
+    return  async (dispatch) => {
+        dispatch(descargarProductos());
+        try{
+            const respuesta = await clienteAxios.get('/productos');
+            dispatch(descargarProductosExitosa(respuesta.data));
+        }catch(err){
+            dispatch(descargarProductosError());
+        }
+    }
+}
+
+const descargarProductos = () => ({
+    type: COMENZAR_DESCARGA_PRODUCTOS,
+    payload: true
+});
+
+const descargarProductosExitosa = productos => ({
+    type: DESCARGA_PRODUCTOS_EXITO,
+    payload: productos
+});
+
+const descargarProductosError = () => ({
+    type: DESCARGA_PRODUCTOS_ERROR,
+    payload: true
 });
--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -1,7 +1,10 @@
 import {
     AGREGAR_PRODUCTO,
     AGREGAR_PRODUCTO_EXITO,
-    AGREGAR_PRODUCTO_ERROR
+    AGREGAR_PRODUCTO_ERROR,
+    COMENZAR_DESCARGA_PRODUCTOS,
+    DESCARGA_PRODUCTOS_EXITO,
+    DESCARGA_PRODUCTOS_ERROR
 } from '../types';
 
 // cada reducer tiene su propio state
@@ -15,6 +18,7 @@ const initialState = {
 export default function (state = initialState, action) {
     switch (action.type) {
         case AGREGAR_PRODUCTO:
+        case COMENZAR_DESCARGA_PRODUCTOS:
             return {
                 ...state,
                 loading: action.payload
@@ -26,10 +30,18 @@ export default function (state = initialState, action) {
                 productos: [...state.productos, action.payload]
             }
         case AGREGAR_PRODUCTO_ERROR:
+        case DESCARGA_PRODUCTOS_ERROR:
             return {
                 ...state,
                 loading: false,
                 error: action.payload
+            }
+        case DESCARGA_PRODUCTOS_EXITO:
+            return {
+                ...state,
+                loading: false,
+                error: null,
+                productos: action.payload
             }
         default:
             return state;
